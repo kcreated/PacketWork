cdde14fca40c9acc127d0618931a22ef26c65fdb
kcreated <kyciabt@gmail.com>
2024-05-14 14:21:32 -0400

    bruv

---
+++ BOX/BoxRunner.java
@@ -0,0 +1,11 @@
+package BoxRunner;
+
+public class BOX{
+    public static void main(String[]args){
+
+        Box bobby = new BOX()
+
+        publix int getArea(){
+            int perm = side + side + side
+        }
+    }
