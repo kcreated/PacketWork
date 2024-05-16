59f41a444cc3cdbd7d2f8c2bac8bb48166dd0d83
kcreated <kyciabt@gmail.com>
2024-05-16 14:55:15 -0400

    heh

---
+++ DICE/Dice.java
@@ -0,0 +1,18 @@
+package DICE;
+
+public class Dice {
+    
+    private int die1, die 2;
+
+    public Dice() {
+
+    }
+    
+    public void roll(){
+
+    }
+
+    public int getTotal(){
+        
+    }
+}
---
+++ DICE/DiceRunner.java
@@ -0,0 +1,14 @@
+package DICE;
+
+public class DiceRunner {
+    public static void main(String[]args){
+        Dice d = new Dice();
+        d.roll();
+        int n = d.getTotal();
+        System.out.println(n);
+
+        d.roll();
+        n = d.getTotal();
+        System.out.println(n);
+    }
+}
