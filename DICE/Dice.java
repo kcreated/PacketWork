db8218a26ee4ee15e808a1d47d392e6ecffabea4
kcreated <kyciabt@gmail.com>
2024-05-17 14:50:23 -0400

    wrerew

--- DICE/Dice.java
+++ DICE/Dice.java
@@ -2,17 +2,21 @@ package DICE;
 
 public class Dice {
     
-    private int die1, die 2;
+    private int die1, die2;
 
     public Dice() {
-
+        die1 = 1;
+        die2 = 4;
     }
     
     public void roll(){
-
+         die1 = (int) (Math.random()*6) + 1;
+         die2 = (int) (Math.random()*6) + 1;
     }
 
     public int getTotal(){
-        
+
+        return die1 + die2;
+
     }
 }
