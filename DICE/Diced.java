f7f14af8d0accd864fb55902244170dc0fb40c15
kcreated <159187385+kcreated@users.noreply.github.com>
2024-05-27 21:01:29 -0400

    YAYYAY
    ahyhheyo!

--- DICE/Diced.java
+++ DICE/Diced.java
@@ -1,22 +1,29 @@
-package DICED;
+package DICE;
 
 public class Diced {
-    
-    private int die1, die2;
 
-    public Dice() {
-        die1 = 1;
-        die2 = 4;
-    }
-    
-    public void roll(){
-         die1 = (int) (Math.random()*100) + 1;
-         die2 = (int) (Math.random()*100) + 1;
-    }
+    public static void main(String[] args) {
+        Dice dice = new Dice();
+        int num7or11 = 0;
+        int num2or3or12 = 0;
+        int numOfRolls = 100;
 
-    public int getTotal(){
+        for (int i = 0; i < numOfRolls; i++) {
+            dice.roll();
+            int total = dice.getTotal();
 
-        return die1 + die2;
+            if (total == 7 || total == 11) {
+                num7or11++;
+            }
+            if (total == 2 || total == 3 || total == 12) {
+                num2or3or12++;
+            }
+        }
 
+        double percentOf7or11 = (num7or11 / (double) numOfRolls) * 100;
+        double percentOf2or3or12 = (num2or3or12 / (double) numOfRolls) * 100;
+
+        System.out.println("You rolled a 7 or 11 \t\t" + percentOf7or11 + "% of the time.");
+        System.out.println("You rolled a 2, 3, or 12 \t" + percentOf2or3or12 + "% of the time.");
     }
 }
